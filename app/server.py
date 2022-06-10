ef934677d4fda9f415634ee94a6a82ffbb1a4615
Andrea Ranieri <andrea.ranieri@cnr.it>
2022-06-10 18:22:25 +0200

    Cleaned up...

--- app/server.py
+++ app/server.py
@@ -51,8 +51,6 @@ from argument_parser import var2opt
 from starlette_auth  import *
 from bcrypt_password import user_pass_db, create_user_pass_db
 
-#classes = [False, True]		# for the ML model
-
 # taken directly from: http://deeplearning.ge.imati.cnr.it/promed/promed-ML-tool-v0.1.html
 model_classes = {
 			'bar'                      : [],
@@ -121,7 +119,6 @@ basepath, userpath = set_paths()
 
 routes = [
 		Route("/admin",	endpoint=admin),
-		#Route("/test",	endpoint=homepage),
 		Route("/",	endpoint=homepage),
 		#Route("/login", endpoint=login),
 		Route('/userdata/{rest_of_path:path}', userdata)
@@ -131,7 +128,6 @@ middleware = [Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())]
 
 app = Starlette(routes=routes, middleware=middleware)
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-#app.mount('/static', StaticFiles(directory='app/static'))
 app.mount('/static', StaticFiles(directory=basepath))
 
 # --------------------------------------------------
@@ -168,23 +164,6 @@ async def setup_learner(cmd, url, model_name):
 			raise RuntimeError(message)
 		else:
 			raise
-'''
-def process_csvdata(csvdata):
-	debug = False
-	if debug:
-		df = pd.read_csv(csvdata, skiprows=KistlerFile.kistler_dataframe_line_offset, sep=';', decimal=',')
-		print(f'{df = }')
-	kf = KistlerFile(fname=csvdata,debug=False)		# let's see if this trick works...
-	if debug:
-		prediction = kf.df
-
-	KistlerFile._kf_max_time = 3.9999			# this is mandatory and hardcoded to obtain (exactly) 800 samples after resampling. ML model expects 800 columns.
-	kf.resample(debug=False)
-
-	sample = create_inference_ready_sample(kf, debug=False)
-	row, clas, probs = learn.predict(sample)
-	return row, clas, probs, kf
-'''
 
 def model_predict(manufacturing_class, debug=True):
 	#manufacturing_class = 'upper_subperiost_implant'
@@ -211,10 +190,8 @@ async def login(request):
 
 def postprocess_model_output(predicted_row, debug=True):
 	if debug:
-		#print(f'{type(predicted_row) = }\n{predicted_row = }\n{clas = }\n{probs = }')
 		df = pd.DataFrame(predicted_row)
 		print(f'{df = }')
-		#print(f'{predicted_row.data = }')
 		print(f'{predicted_row.items = }')
 		print(f'{predicted_row.x_names = }')
 		print(f'{predicted_row.y = }')
@@ -225,7 +202,6 @@ def postprocess_model_output(predicted_row, debug=True):
 	if debug:
 		print(f'{manufacturing_class  = }')
 		print(f'{df_data = }')
-		#print(f'{df_data[1] = }')
 
 	html_output	= f'manufacturing_class: {manufacturing_class} -> '
 	post_req_output	= ''
@@ -240,16 +216,13 @@ def postprocess_model_output(predicted_row, debug=True):
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
 	form_data = await request.form()
-	#starlette_data = await(form_data['file'].read())
-	starlette_data = form_data['manufacturing-class']
-	#csvdata = StringIO(str(starlette_data))
+	content = form_data['manufacturing-class']
 
 	debug = True
 	if debug:
-		print(f'Predicting: {starlette_data = }') 
+		print(f'Predicting: {content = }') 
 
-	#row, clas, probs, kf = process_csvdata(csvdata)
-	row, clas, probs = model_predict(starlette_data, debug=debug)
+	row, clas, probs = model_predict(content, debug=debug)
 	if debug:
 		print(f'{type(row) = }\n{row = }\n{clas = }\n{probs = }')
 
@@ -290,43 +263,6 @@ def result():
 	if not password_verified:
 		return f"/post received username: {username} -> {msg}{username}"
 
-	'''
-	print(f'Received filename: {Path(request.form["filename"]).stem}\n\n')
-	content = request.form['kistlerfile']
-	readable_hash = hashlib.sha256(content.encode('utf-8')).hexdigest();
-
-	csvdata = StringIO(str(content))
-
-	row, clas, probs, kf = process_csvdata(csvdata)
-	predstr = str(classes[int(clas)])
-
-	print(f'\n\nPrediction for filename: {Path(request.form["filename"]).stem} -> class: {predstr} -> probs: {str(probs)}\n\n')
-
-	graphfn     = userpath / 'graph' / (Path(request.form["filename"]).stem + '.png')
-	resampledfn = userpath / 'graph' / (Path(request.form["filename"]).stem + '-resampled.png')
-	csvfn       = userpath / 'data'  / (Path(request.form["filename"]).name)
-	htmlfn      = userpath / 'html'  / (Path(request.form["filename"]).stem + '.html')
-
-	print(f'\nWriting graph to: {graphfn}, CSV file to {csvfn} and HTML file to {htmlfn}')
-
-	graphfn.parent.mkdir(exist_ok=True, parents=True)
-	csvfn.parent.mkdir(exist_ok=True, parents=True)
-	htmlfn.parent.mkdir(exist_ok=True, parents=True)
-
-	kf.graph(resampled=False, both=True, debug=False, filename=graphfn)
-
-	with open(csvfn, 'w') as fd:
-		csvdata.seek(0)
-		shutil.copyfileobj(csvdata, fd)
-
-	graphfnlink	= Path('..') / Path(*graphfn.parts[3:])		# remove leading '/app/static' (because our relative path is deeplearning.ge.imati.cnr.it:55564/static/html)
-	csvfnlink	= Path('..') / Path(*csvfn.parts[3:])		# remove leading '/app/static' (because our relative path is deeplearning.ge.imati.cnr.it:55564/static/html)
-	print(f'Writing links: {graphfnlink} - {csvfnlink}')
-	template    = open(Path('/') / basepath / 'template.html').read().format(csv_fn=csvfnlink, graph_fn=graphfnlink, pred=predstr)
-
-	with open(htmlfn, "w") as html_file:
-		html_file.write(template)
-	'''
 	print(f'Received manufacturing-class: {Path(request.form["manufacturing-class"]).stem}\n\n')
 	content = request.form['manufacturing-class']
 	if debug:
